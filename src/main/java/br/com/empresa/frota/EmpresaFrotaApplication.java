dfe04ed72a49347bc00c96b60c1c7676e4929ff0
Jonas <ferreirabentojonas@gmail.com>
2022-09-15 14:40:03 -0300

    first commit

---
+++ src/main/java/br/com/empresa/frota/EmpresaFrotaApplication.java
@@ -0,0 +1,13 @@
+package br.com.empresa.frota;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmpresaFrotaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmpresaFrotaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/empresa/frota/EmpresaFrotaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.empresa.frota;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmpresaFrotaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
