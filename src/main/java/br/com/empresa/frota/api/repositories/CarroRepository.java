0d489a657a65a58e6a80c9d8ef0c31d9f4e3c1bc
Jonas <ferreirabentojonas@gmail.com>
2022-10-13 15:22:51 -0300

    Commit dia 13/10/2022

--- src/main/java/br/com/empresa/frota/api/repositories/CarroRepository.java
+++ src/main/java/br/com/empresa/frota/api/repositories/CarroRepository.java
@@ -1,5 +1,26 @@
 package br.com.empresa.frota.api.repositories;
 
-public interface CarroRepository {
+	import java.util.List;
 
+	import javax.persistence.NamedQueries;
+	import javax.persistence.NamedQuery;
+
+	import org.springframework.data.domain.Page;
+	import org.springframework.data.domain.Pageable;
+	import org.springframework.data.jpa.repository.JpaRepository;
+	import org.springframework.data.repository.query.Param;
+	import org.springframework.transaction.annotation.Transactional;
+
+	import br.com.empresa.frota.api.entities.Carro;
+
+	@Transactional(readOnly = true)
+	@NamedQueries({
+			@NamedQuery(name = "LancamentoRepository.findByFuncionarioId", 
+					query = "SELECT lanc FROM Lancamento lanc WHERE lanc.funcionario.id = :funcionarioId") })
+	public interface CarroRepository extends JpaRepository<Carro, Long> {
+
+		List<Carro> findByFuncionarioId(@Param("funcionarioId") Long funcionarioId);
+
+		Page<Carro> findByFuncionarioId(@Param("funcionarioId") Long funcionarioId, Pageable pageable);
 }
+
